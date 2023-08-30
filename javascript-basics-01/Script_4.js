8f81db083a8e949ab6cb1929d73ed3b58feda7d0
Fuad Abou Heit <f.abouheit@gmail.com>
2023-08-30 10:43:57 +0300

    completed step 4

---
+++ javascript-basics-01/Script_4.js
@@ -0,0 +1,13 @@
+// var name = prompt("What is your name?");
+// var surname = prompt("What is your surname?");
+// var city = prompt("Where do you live?");
+
+// window.alert("Hello " + name + " " + surname + " from " + city);
+
+function formfunction(){
+    let name = document.forms["formname"]["name"].value;
+    let surname = document.forms["formname"]["surname"].value;
+    let city = document.forms["formname"]["city"].value;
+
+    window.alert("Hello " + name + " " + surname + " from " + city );
+}
