1c463719639487863d86707828c4b8037caf0b5f
Fuad Abou Heit <f.abouheit@gmail.com>
2023-08-30 10:20:22 +0300

    completed step 2

---
+++ javascript-basics-01/Step_2.js
@@ -0,0 +1,9 @@
+let name = "John";
+let surname = "Doe";
+let city = "New-York";
+
+function myfunction(){
+    window.alert("Nom : " + surname + "\nPrenom : " + name + "\nVille : "+ city);
+}
+
+myfunction();
