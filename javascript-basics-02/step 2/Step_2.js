7aa8d5767cdf2bb535bae3ed654d467ca92fc084
Fuad Abou Heit <f.abouheit@gmail.com>
2023-08-31 06:18:26 +0300

    completed step 2 js 2

---
+++ javascript-basics-02/step 2/Step_2.js
@@ -0,0 +1,14 @@
+// window.addEventListener('load', (event) => {
+//     window.alert('The page has fully loaded');
+// });
+
+
+// const element = document.querySelector("input");
+// const element = document.getElementById("name");
+// element.addEventListener("blur",theFunction());
+// element.onblur = theFunction();
+
+function theFunction(){
+    window.alert("Thank you for participating!");
+}
+
