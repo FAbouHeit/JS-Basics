c9772535a9d23c6ee790d7fae151e07e120b27ba
Fuad Abou Heit <f.abouheit@gmail.com>
2023-08-31 13:18:16 +0300

    completed step 5 js 2 no html edit

--- javascript-basics-02/step 4/Step_4.js
+++ javascript-basics-02/step 4/Step_4.js
@@ -1,16 +1,21 @@
-let button = document.getElementsByName("button");
-button[0].onclick = function() {myFunction()};
+let inputs = document.querySelectorAll('input')
+let button = document.querySelector("button");
+button.addEventListener('click', () =>{
+    inputs.forEach((element) => {
+        element.value = ''
+    })
+} );
 
-function myFunction(){
-    let y = document.querySelectorAll("input");
+// function myFunction(){
+//     let y = document.querySelectorAll("input");
 
-    if (window.confirm("Do you really want to reset?")) {
-        // window.open("exit.html", "Thanks for Visiting!");
-        for(let x=0; x<3;x++){
-            y[x].value ="";
-        }
-      }
+//     if (window.confirm("Do you really want to reset?")) {
+//         // window.open("exit.html", "Thanks for Visiting!");
+//         for(let x=0; x<3;x++){
+//             y[x].value ="";
+//         }
+//       }
 
-}
+// }
 
 
---
+++ javascript-basics-02/step 5/Step_5.js
@@ -0,0 +1,25 @@
+
+let images = document.querySelectorAll("img");
+console.log(images)
+images.forEach((a) => {
+    a.addEventListener("mouseover", () =>{
+    a.src = `images/${a.id}_2.jpg`
+})
+})
+
+// let source ="";
+// let str = "";
+// let prev = "";
+
+// for(let x = 0; x < 5; x++){
+//         str = x.toString();
+    
+//         source = "..images/image" + x+"_"+x+".jpg";
+//     images[x].addEventListener("mouseover", () =>{
+
+//         images[x].setAttribute('src', source);
+//         console.log(images[x].src);
+//     })
+// }
+
+// "images/image2_2.jpg"
