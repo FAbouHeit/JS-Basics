f6b67d95c7b168533302732cad12657839449f0b
Fuad Abou Heit <f.abouheit@gmail.com>
2023-08-31 05:51:00 +0300

    completed step 1 JS 2

---
+++ javascript-basics-02/step 1/Step_1.js
@@ -0,0 +1,9 @@
+document.querySelector("img").addEventListener("mouseover", myFunction);
+
+function myFunction(){
+    var x = document.getElementById('image1');
+
+    x.style.height = "120%";
+    x.style.width = "120%";
+    
+}
