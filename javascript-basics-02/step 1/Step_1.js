55b0c3976ac08e6daac1ba3ec3d99d2341208eba
Fuad Abou Heit <f.abouheit@gmail.com>
2023-09-01 10:18:37 +0300

    finished step 1 basics 2 reverted html

--- javascript-basics-02/step 1/Step_1.js
+++ javascript-basics-02/step 1/Step_1.js
@@ -1,9 +1,11 @@
-document.querySelector("img").addEventListener("mouseover", myFunction);
+let image = document.querySelector("img");
+image.addEventListener("mouseover", myFunction);
 
 function myFunction(){
     var x = document.getElementById('image1');
 
-    x.style.height = "120%";
-    x.style.width = "120%";
+    // x.style.height = "120%";
+    // x.style.width = "120%";
+    image.src = "images/image1_2.jpg"
     
 }
