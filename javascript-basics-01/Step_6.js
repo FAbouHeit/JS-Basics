abae3c4a17f09299cb51a79fc27ba17bc418e3d5
Fuad Abou Heit <f.abouheit@gmail.com>
2023-09-01 09:54:49 +0300

    completed basics 1 step 6 reverted html

--- javascript-basics-01/Step_6.js
+++ javascript-basics-01/Step_6.js
@@ -1,7 +1,16 @@
+let a = document.getElementById("first_number");
+let b = document.getElementById("second_number");
+let button = document.getElementById("validate");
+
+button.addEventListener("click", ()=>{
+    my_function();
+})
+
+
 function my_function(){
-    let first_number = document.forms["myform"]["first_number"].value;
-    let second_number = document.forms["myform"]["second_number"].value;
-    let value = first_number%second_number;
+    // let first_number = document.forms["myform"]["first_number"].value;
+    // let second_number = document.forms["myform"]["second_number"].value;
+    let value = a.value%b.value;
 
     // window.alert(first_number + "x" + second_number + " is " + value);
     window.alert(value);
