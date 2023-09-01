e3daf71e022a6d31029dd336532e3aa100b2ceed
Fuad Abou Heit <f.abouheit@gmail.com>
2023-09-01 10:08:04 +0300

    finished step 7 reverted html basics 1

--- javascript-basics-01/Step_7.js
+++ javascript-basics-01/Step_7.js
@@ -3,24 +3,34 @@ window.alert("Please enter your shoe size and year of birth :)");
 // let first_number;
 // let second_number;
 
+let a = document.getElementById("shoe_size");
+let b = document.getElementById("year");
+let button = document.getElementById("validate");
+
+button.addEventListener("click", ()=>{
+    // theFunction();
+    let num = (((a.value *2)+5)*50)-b.value +1776;
+    window.alert(num);
+})
 
 
-function theFunction(){
+// function theFunction(){
 
-    let first_number = document.forms["myform"]["shoe_size"].value;
-    let second_number = document.forms["myform"]["year"].value;
+        // console.log(a.value)
+    // let first_number = document.forms["myform"]["shoe_size"].value;
+    // let second_number = document.forms["myform"]["year"].value;
     
-    let num = (((first_number *2)+5)*50)-second_number +1776;
+   
     // num+=5;
     // num*=50;
     // num-=two;
     // num+=1776;
-    if(first_number!=0){
-    window.alert(num);
-    }
-}
+    // if(a.value!=0){
+    
+    // }
+// }
 
 
 
 
-theFunction();
+// theFunction();
